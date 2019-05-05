0c681ebafe0bfe1a794bafffb6d20de6152112bc
BenFeiJr <benfeijr@163.com>
2019-05-05 21:22:03 +0800

    feat: Promise a+ 标准实现 v0.3
    已实现：
    1. Promise A+标准的 The Promise Resolution Procedure 部分
    
    未实现：
    1. Promise A+标准的 2.3.3.3.3 和 2.3.3.3.4

--- demo/promise.js
+++ demo/promise.js
@@ -3,6 +3,7 @@
  */
 
 const MyPromise = function (executor) {
+    const promiseInstance = this;
     const PENDING = 'pending';
     const FULFILLED = 'fulfilled';
     const REJECTED = 'rejected';
@@ -31,58 +32,112 @@ const MyPromise = function (executor) {
         return isObject(value) && Object.prototype.toString.call(value).toLowerCase() === '[object function]';
     }
 
-    let promiseState = PENDING;
-    let fulfilledValue, rejectedReason;
+    promiseInstance._state = PENDING;
 
     const resolve = function (value) {
         // 更改状态
         // 发布事件
-        promiseState = FULFILLED;
-        fulfilledValue = value;
+        promiseInstance._state = FULFILLED;
+        promiseInstance._fulfilledValue = value;
         pubsub.fire(FULFILLED);
     };
 
     const reject = function (reason) {
-        promiseState = REJECTED;
-        rejectedReason = reason;
+        promiseInstance._state = REJECTED;
+        promiseInstance._rejectedReason = reason;
         pubsub.fire(REJECTED);
     };
+
+    const promiseResolutionProcedure = function (promise2, promise2_resolve, promise2_reject, promise1_value) {
+        if (promise1_value instanceof MyPromise) {
+            const promise3 = promise1_value;
+            switch (promise3._state) {
+                case PENDING: {
+                    promise3.then.call(
+                        promise1_value,
+                        function (promise3_value) {
+                            promiseResolutionProcedure(promise2, promise2_resolve, promise2_reject, promise3_value);
+                        },
+                        function (promise3_reason) {
+                            promise2_reject(promise3_reason);
+                        }
+                    );
+                    break;
+                }
+                case FULFILLED: {
+                    promise2_resolve(promise3._fulfilledValue);
+                    break;
+                }
+                case REJECTED: {
+                    promise2_reject(promise3._rejectedReason);
+                    break;
+                }
+            }
+        }
+        else if (isObject(promise1_value)) {
+            try {
+                if (isFunction(promise1_value.then)) {
+                    const promise3 = promise1_value;
+                    promise3.then.call(
+                        promise1_value,
+                        function (promise3_value) {
+                            promiseResolutionProcedure(promise2, promise2_resolve, promise2_reject, promise3_value);
+                        },
+                        function (promise3_reason) {
+                            promise2_reject(promise3_reason);
+                        }
+                    );
+                }
+                else {
+                    promise2_resolve(promise1_value);
+                }
+            }
+            catch (err) {
+                promise2_reject(err);
+            }
+        }
+        else {
+            promise2_resolve(promise1_value);
+        }
+    }
 
     const then = function (onFulfilled, onRejected) {
         // 注册事件
         // 监听状态，调用事件
 
-        return new MyPromise((resolve, reject) => {
+        const promise2 = new MyPromise((resolve, reject) => {
             pubsub.on(FULFILLED, () => {
                 if  (isFunction(onFulfilled)) {
                     try {
-                        const value = onFulfilled(fulfilledValue);
-                        resolve(value);
+                        const value = onFulfilled(promiseInstance._fulfilledValue);
+                        promiseResolutionProcedure(promise2, resolve, reject, value);
                     }
                     catch (err) {
                         reject(err);
                     }
                 }
                 else {
-                    resolve(fulfilledValue);
+                    resolve(promiseInstance._fulfilledValue);
                 }
             });
 
             pubsub.on(REJECTED, () => {
                 if (isFunction(onRejected)) {
                     try {
-                        const value = onRejected(rejectedReason);
-                        resolve(value);
+                        const value = onRejected(promiseInstance._rejectedReason);
+                        promiseResolutionProcedure(promise2, resolve, reject, value);
                     }
                     catch (err) {
                         reject(err);
                     }
                 }
                 else {
-                    reject(rejectedReason);
+                    reject(promiseInstance._rejectedReason);
                 }
             });
         });
+
+        return promise2;
     };
 
     executor(resolve, reject);
@@ -92,10 +147,17 @@ const MyPromise = function (executor) {
 
 const p1 = new MyPromise((resolve, reject) => {
     setTimeout(() => {
-        resolve(12);
+        resolve(1);
     }, 3000)
 });
 
-p1.then().then((res) => {
+p1.then((res) => {
+    console.log(res);
+    return new MyPromise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(2);
+        }, 3000)
+    })
+}).then((res) => {
     console.log(res);
 });
